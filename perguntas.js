dc80576bf68caf1d0e79f76a8a17eb28b44a0dba
lucasferrerado2A <00001105027727SP@al.educacao.sp.gov.br>
2024-11-08 12:05:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'darkrise',
+    'o que é o darkrise?',
+    'Darkrise é um jogo hardcore clássico que foi criado por dois desenvolvedores indie no estilo nostálgico de pixel.'
+)
+
+criaCartao(
+    'História'
+
+    'Neste jogo de RPG de ação você pode conhecer 3 personagens Alice Godric e Kira'
+
+    'Cada um deles tem habilidades mecânica de jogo e recursos únicos; A pátria dos heróis do jogo foi invadida por criaturas mortas-vivas e demônios. Agora eles precisam se tornar mais fortes e limpar o país dos invasores   Existem 20 locais para jogar e 6 dificuldades Os inimigos irão surgir à sua frente ou aparecer em portais que irão surgir aleatoriamente no local a cada poucos segundos Todos os inimigos são diferentes e têm suas características únicas Inimigos defeituosos podem aparecer às vezes, eles têm estatísticas aleatórias e você não pode prever seus poderes É por isso que jogar Darkrise nunca é entediante'
+
+)
+
+criaCartao(
+    'gameplay',
+    'como é o jogo de camera?',
+    'O sistema de combate é bastante interessante: a câmera treme, flashes de ataque, animação de queda de saúde, queda voando por todos os lados. Seu personagem e inimigos são rápidos, você sempre tem que se mover se não quiser perder.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
